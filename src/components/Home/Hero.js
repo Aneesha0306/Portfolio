9db8d2def50a242a93c07b419f9040b5fd0f7e84
Aneesha0306 <aneeshainaction@gmail.com>
2024-12-30 12:08:50 +0530

    fin

--- src/components/Home/Hero.js
+++ src/components/Home/Hero.js
@@ -15,9 +15,10 @@ function Hero() {
                 <div className="text-left">
                     <div className="text-2xl font-semibold mb-4 ml-[50px] mr-[40px] hover:scale-110 transition-transform duration-300">My name is Lakshmi Aneesha Ambati</div>
 
-                    <div className='ml-[50px] mr-[40px] font-semibold text-xl' style={{ color: 'rgb(47, 49, 55)' }}>At the Forefront of the Courier Industry, We Seamlessly Connect Businesses
-                        and Individuals Across the Globe. Our Fleet of Modern Vehicles, Skilled Drivers,
-                        and Real- Time Tracking Ensure Your Packages Arrive Safely and</div>
+                    <div className='ml-[50px] mr-[40px] font-semibold text-xl' style={{ color: 'rgb(47, 49, 55)' }}>
+                    Hi, I'm Lakshmi Aneesha Ambati, currently studying Computer Science. I'm still learning the ropes, 
+                    but I'm excited to tackle new challenges and grow my skills in tech. Welcome to my space — it's a work in progress, 
+                    just like me!</div>
                 </div>
             </div>
     </div>
